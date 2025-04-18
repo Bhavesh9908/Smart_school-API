1ba95f1ce2ca90023cf1d9510d1af5c937f1f3e0
Bhavesh9908 <vu4f2122023@pvppcoe.ac.in>
2025-04-18 17:59:10 +0530

    change app

--- app.py
+++ app.py
@@ -190,5 +190,7 @@ def calculate_nutrition():
 def uploaded_file(filename):
     return send_from_directory("uploads", filename)
 
+#if __name__ == "__main__":
+ #   app.run(debug=True, port=10000)
 if __name__ == "__main__":
-    app.run(debug=True, port=10000)
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
